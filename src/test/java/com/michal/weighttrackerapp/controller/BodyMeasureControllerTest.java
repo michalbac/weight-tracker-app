51f1bb5e722ce81d104830bfc6d6b82e82c9bc2d
Michal Bac <michal.bac1993@gmail.com>
2021-02-13 16:36:27 +0100

    Body measure controller testing - post

--- src/test/java/com/michal/weighttrackerapp/controller/BodyMeasureControllerTest.java
+++ src/test/java/com/michal/weighttrackerapp/controller/BodyMeasureControllerTest.java
@@ -1,30 +1,36 @@
 package com.michal.weighttrackerapp.controller;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebRequest;
+import com.gargoylesoftware.htmlunit.WebResponse;
+import com.gargoylesoftware.htmlunit.html.*;
+import com.gargoylesoftware.htmlunit.util.NameValuePair;
 import com.michal.weighttrackerapp.domain.BodyMeasure;
 import com.michal.weighttrackerapp.domain.UserAccount;
 import com.michal.weighttrackerapp.repository.UserRepository;
-import com.michal.weighttrackerapp.security.SecurityConfiguration;
 import com.michal.weighttrackerapp.service.BodyMeasureService;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.htmlunit.MockMvcWebClientBuilder;
+import org.springframework.web.context.WebApplicationContext;
 
-import javax.validation.constraints.NotNull;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import java.util.ArrayList;
@@ -32,7 +38,6 @@ import java.util.Date;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
@@ -48,6 +53,15 @@ public class BodyMeasureControllerTest {
 
     @MockBean
     private UserRepository userRepository;
+
+    WebClient webClient;
+
+    @BeforeEach
+    void setup(WebApplicationContext context) {
+        webClient = MockMvcWebClientBuilder
+                .webAppContextSetup(context, springSecurity())
+                .build();
+    }
 
 
     @Test
@@ -102,44 +116,76 @@ public class BodyMeasureControllerTest {
         user.setId(1);
         user.setEmail("test@com.pl");
         user.setUserName("test");
-        Date date = new Date(System.currentTimeMillis());
-        BodyMeasure bm1 = new BodyMeasure(1, 30, 30, 90, 100,
-                60, 60, date, user);
         when(userRepository.findByUserName(any())).thenReturn(user);
+        HtmlPage page = webClient.getPage("http://localhost:8080/body/new");
+        HtmlForm form = page.getHtmlElementById("form");
+        HtmlTextInput rbInput = page.getHtmlElementById("rightBicep");
+        rbInput.setValueAttribute("30");
+        HtmlTextInput lbInput = page.getHtmlElementById("leftBicep");
+        lbInput.setValueAttribute("30");
+        HtmlTextInput waistInput = page.getHtmlElementById("waist");
+        waistInput.setValueAttribute("90");
+        HtmlTextInput chestInput = page.getHtmlElementById("chest");
+        chestInput.setValueAttribute("100");
+        HtmlTextInput ltInput = page.getHtmlElementById("leftThigh");
+        ltInput.setValueAttribute("60");
+        HtmlTextInput rtInput = page.getHtmlElementById("rightThigh");
+        rtInput.setValueAttribute("60");
+        HtmlTextInput dateInput = page.getHtmlElementById("db4");
+        dateInput.setValueAttribute("2021-02-13");
+        HtmlButton submit = form.getButtonByName("submit");
 
         //When & Then
-        String rbParamName = "rightBicep";
-        String lbParamName = "leftBicep";
-        String waistParamName = "waist";
-        String chestParamName = "chest";
-        String ltParamName = "leftThigh";
-        String rtParamName = "rightThigh";
-        String db4ParamName = "db4";
-
-        mockMvc.perform(get("/body/new"))
-                .andExpect(xpath("//input[@name='" + rbParamName + "']").exists())
-                .andExpect(xpath("//input[@name='" + lbParamName + "']").exists())
-                .andExpect(xpath("//input[@name='" + waistParamName + "']").exists())
-                .andExpect(xpath("//input[@name='" + chestParamName + "']").exists())
-                .andExpect(xpath("//input[@name='" + ltParamName + "']").exists())
-                .andExpect(xpath("//input[@name='" + rtParamName + "']").exists())
-                .andExpect(xpath("//input[@name='" + db4ParamName + "']").exists());
+        WebResponse repsonse = submit.click().getWebResponse();
+        assertThat(repsonse.getStatusCode()).isEqualTo(200);
+        System.out.println(repsonse.getContentAsString());
 
 
 
-        MockHttpServletRequestBuilder createMeasure = post("/body/save")
-                .characterEncoding("UTF-8")
-                .param("rightBicep", "30")
-                .param("leftBicep", "30")
-                .param("waist", "90")
-                .param("chest", "100")
-                .param("leftThigh", "60")
-                .param("rightThigh", "60")
-                .param("db4", String.valueOf(date));
 
-        mockMvc.perform(createMeasure)
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/body"));
+
+//        UserAccount user = new UserAccount();
+//        user.setId(1);
+//        user.setEmail("test@com.pl");
+//        user.setUserName("test");
+//        Date date = new Date(System.currentTimeMillis());
+//        BodyMeasure bm1 = new BodyMeasure(1, 30, 30, 90, 100,
+//                60, 60, date, user);
+//        when(userRepository.findByUserName(any())).thenReturn(user);
+//
+//        //When & Then
+//        String rbParamName = "rightBicep";
+//        String lbParamName = "leftBicep";
+//        String waistParamName = "waist";
+//        String chestParamName = "chest";
+//        String ltParamName = "leftThigh";
+//        String rtParamName = "rightThigh";
+//        String db4ParamName = "db4";
+//
+//        mockMvc.perform(get("/body/new"))
+//                .andExpect(xpath("//input[@name='" + rbParamName + "']").exists())
+//                .andExpect(xpath("//input[@name='" + lbParamName + "']").exists())
+//                .andExpect(xpath("//input[@name='" + waistParamName + "']").exists())
+//                .andExpect(xpath("//input[@name='" + chestParamName + "']").exists())
+//                .andExpect(xpath("//input[@name='" + ltParamName + "']").exists())
+//                .andExpect(xpath("//input[@name='" + rtParamName + "']").exists())
+//                .andExpect(xpath("//input[@name='" + db4ParamName + "']").exists());
+
+
+
+//        MockHttpServletRequestBuilder createMeasure = post("/body/save")
+//                .characterEncoding("UTF-8")
+//                .param("rightBicep", "30")
+//                .param("leftBicep", "30")
+//                .param("waist", "90")
+//                .param("chest", "100")
+//                .param("leftThigh", "60")
+//                .param("rightThigh", "60")
+//                .param("db4", String.valueOf(date));
+//
+//        mockMvc.perform(createMeasure)
+//                .andExpect(status().is3xxRedirection())
+//                .andExpect(redirectedUrl("/body"));
 
     }
 }
